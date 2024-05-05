7fd2f54347438dc21b647b1e4d604d9c271d5323
Angie Cristancho <angiecristanch@gmail.com>
2024-05-05 00:11:47 -0500

    Incluye la clase trapecio y un ejemplo de su uso

---
+++ Trapecio.h
@@ -0,0 +1,24 @@
+#ifndef TRAPECIO_H
+#define TRAPECIO_H
+
+#include <string>
+#include <vector>
+#include "Termino.h"
+
+class Trapecio: public Termino {
+    private:
+        float x1, x2, x3, x4;
+
+    public:
+        Trapecio(float h, std::string nombre, std::vector<std::string> coor);
+        void setX1(float x1);
+        void setX2(float x2);
+        void setX3(float x3);
+        void setX4(float x4);
+        void setConjuntoFuzzy(std::vector<std::string> c) override;
+        float pertenenciaCF(float i) override;
+        std::string getCoordenadas() override;
+};
+
+#endif TRAPECIO_H
+
