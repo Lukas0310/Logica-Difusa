e6b4554ccdc486009f3d2849b1e50d9f371d1f1a
angie-cris <168344956+angie-cris@users.noreply.github.com>
2024-05-06 22:10:01 -0500

    Comentando

--- Termino.h
+++ Termino.h
@@ -5,10 +5,18 @@
 #include <vector>
 
 class Termino {
+/* la clas termino es una clase padre, basicamente se refiere a cada conjunto fuzzy o
+    termino linguistico que tenga una variable. En el ejemplo estos son los valores de
+    "muy frio", "muy feo", "normal" donde el conjunto puede ser un triangulo o 
+    un trapecio
+*/
     private:
+        /* tiene como atributos el nombre, el valor de pertenencia que se calcula ocn un metodo que 
+        se puede heredar de forma diferente para triangulo o para trapecio
+        */
         std::string nombreT;
         float pertenencia;
-        char tipo; //triangulo T o trapezoide Z
+        char tipo; //triangulo T o trapezoide Z aunque no lo usamos
         float altura; //se usa para la parte de inferencia y defuzzificacion
 
     public:
@@ -20,6 +28,8 @@ class Termino {
         float getPertenencia();
         char getTipo();
         float getAltura();
+        //estos son los metodos que se pueden heredar a los tipos de termino
+        //si es triangulo es diferente a si es trapecio y por eso lo de calcularPertenencias cambia
         virtual void setConjuntoFuzzy(std::vector<std::string> p ) = 0;
         virtual std::string getCoordenadas() = 0;
         virtual float calcularPertenencia(float input) = 0 ;
