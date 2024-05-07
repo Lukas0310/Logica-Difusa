f07f807d8cdb669529bd99378fb6a018fa9c2391
angie-cris <168344956+angie-cris@users.noreply.github.com>
2024-05-06 22:16:55 -0500

    comentando

--- Trapecio.h
+++ Trapecio.h
@@ -6,6 +6,11 @@
 #include "Termino.h"
 
 class Trapecio: public Termino {
+    /* la clase trapecio hereda termino porque es un tipo de conjunto fuzzy
+    en este caso tenemos 4 atributos haciendo referencia a las coordenadas en x del trapecio
+        solo las tomamos en x porque asumimos que x2 y x3 están en el punto más alto i.e. 1
+    también hereda los métodos de calcularPertenencia y los adapta
+    */
     private:
         float x1, x2, x3, x4;
 
@@ -15,7 +20,9 @@ class Trapecio: public Termino {
         void setX2(float x2);
         void setX3(float x3);
         void setX4(float x4);
+        //metodos heredados
         void setConjuntoFuzzy(std::vector<std::string> c) override;
+        //en este caso configura cuatro coordenadas en vez de tres
         float calcularPertenencia(float i) override;
         std::string getCoordenadas() override;
         bool estaDentroDelIntervalo(float i) override;
