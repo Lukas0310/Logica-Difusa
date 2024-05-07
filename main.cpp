84e3ce84360473c9cc2b0653cadbf5b6228d71e6
angie-cris <168344956+angie-cris@users.noreply.github.com>
2024-05-06 20:49:37 -0500

    Ejemplo de las diapositivas: declaracion de variables

--- main.cpp
+++ main.cpp
@@ -9,56 +9,68 @@ std::vector<Variable*> variables;
 //using namespace std;
 
 int main() {
+
+    //declaracion de variables del ejemplo de las diapositivas
     std::string nVar1 = "Temperatura";
-    //std::string dVar1 = "Descripcion de la variable (eliminar de atributos de clase)";
+    std::string dVar1 = "Descripcion de la variable (eliminar de atributos de clase)";
     float minValVar1 = 110;
     float maxValVar1 = 330;
     std::string nFrio = "Frio";
     std::vector<std::string> cFrio = {"110", "110", "140", "180"};
     std::string nFresco = "Fresco";
     std::vector<std::string> cFresco = {"135", "180", "220"};
-    std::string nFrio = "Normal";
+    std::string nNormal = "Normal";
     std::vector<std::string> cNormal = {"180", "220", "260"};
-    std::string nFrio = "Tibio";
+    std::string nTibio = "Tibio";
     std::vector<std::string> cTibio = {"225", "255", "280"};
-    std::string nFrio = "Caliente";
+    std::string nCaliente = "Caliente";
     std::vector<std::string> cCaliente = {"260", "280", "330", "330"};
+    Trapecio* ptrFrio =  new Trapecio(nFrio, cFrio);
+    Trapecio* ptrCaliente = new Trapecio(nCaliente, cCaliente);
+    Triangulo* ptrFresco = new Triangulo(nFresco, cFresco);
+    Triangulo* ptrNormal = new Triangulo(nNormal, cNormal);
+    Triangulo* ptrTibio = new Triangulo(nTibio, cTibio);
+    std::vector<Termino*> terminosLing;
+    terminosLing.push_back(ptrFrio);
+    terminosLing.push_back(ptrCaliente);
+    terminosLing.push_back(ptrNormal);
+    terminosLing.push_back(ptrTibio);
+    terminosLing.push_back(ptrFresco);
+    Variable* ptrTemperatura = new Variable(nVar1, dVar1, terminosLing, minValVar1, maxValVar1);
+
+    std::string nVar2 = "Presion";
+    std::string dVar2 = "Descripcion de la variable (eliminar de atributos de clase)";
+    float minValVar2 = 10;
+    float maxValVar2 = 230;
+    std::string nDebil = "Debil";
+    std::vector<std::string> cDebil = {"10", "10", "30", "70"};
+    std::string nBaja = "Baja";
+    std::vector<std::string> cBaja = {"28", "70", "120"};
+    std::string nNormal2 = "Normal";
+    std::vector<std::string> cNormal2 = {"70", "120", "165"};
+    std::string nFuerte = "Fuerte";
+    std::vector<std::string> cFuerte = {"125", "165", "200"};
+    std::string nAlta = "Alta";
+    std::vector<std::string> cAlta = {"165", "200", "230", "230"};
+    Trapecio* ptrDebil =  new Trapecio(nDebil, cDebil);
+    Trapecio* ptrAlta = new Trapecio(nAlta, cAlta);
+    Triangulo* ptrBaja = new Triangulo(nBaja, cBaja);
+    Triangulo* ptrNormal2 = new Triangulo(nNormal2, cNormal2);
+    Triangulo* ptrFuerte = new Triangulo(nFuerte, cFuerte);
+    std::vector<Termino*> terminosLing2;
+    terminosLing2.push_back(ptrDebil);
+    terminosLing2.push_back(ptrAlta);
+    terminosLing2.push_back(ptrBaja);
+    terminosLing2.push_back(ptrNormal2);
+    terminosLing2.push_back(ptrFuerte);
+    Variable* ptrPresion = new Variable(nVar2, dVar2, terminosLing2, minValVar2, maxValVar2);
+
+    variables.push_back(ptrTemperatura);
+    variables.push_back(ptrPresion);
+    
+    //declaracion de reglas del ejemplo de las diapositivas
     
     
-    Variable* ejemploVar1 = new Variable();
-    std::vector<std::string> c1 ={"0", "20","43"};
-    std::string n1 = "Lluvia";
-    Triangulo* ptrTr1 = new Triangulo(n1, c1);
 
-    std::vector<std::string> c2 ={"10", "30","50", "70"};
-    std::string n2 = "Niebla";
-    Trapecio* ptrTr2 = new Trapecio(n2, c2);
-
-    std::vector<std::string> c3 ={"25", "50","80"};
-    std::string n3 = "Sol";
-    Triangulo* ptrTr3 = new Triangulo(n3, c3);
-
-
-    /*
-
-    std::cout<<ptrTr1->calcularPertenencia(15.0)<<std::endl;
-    std::cout<<ptrTr2->calcularPertenencia(30.0)<<std::endl;
-    std::cout<<ptrTr3->calcularPertenencia(15.0)<<std::endl;
-
-    */
-
-    std::vector<Termino*> terminos;
-    terminos.push_back(ptrTr1);
-    terminos.push_back(ptrTr2);
-    terminos.push_back(ptrTr3);
-
-    std::string varNombre = "Clima";
-    std::string varDescripcion = "El valor del clima de hoy";
-    float climaMin = 0.0;
-    float climaMax = 80.0;
-    //los valores de prueba son aleatorios
-
-    Variable varPrueba(varNombre, varDescripcion, terminos, climaMin, climaMax);
-    varPrueba.fuzificar(44.0);
     return 0;
 }
